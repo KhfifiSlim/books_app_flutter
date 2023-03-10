75bd6154cbbf1d7c30d6ebbeb6aa1d5106c94d71
khfifi_slim <khfifislim@gmail.com>
2023-03-10 22:37:58 +0100

    Initial commit

---
+++ lib/ListBook.dart
@@ -0,0 +1,31 @@
+import 'Book.dart';
+
+List<Book> books = [
+   Book(
+      title: 'Le Dit du Genji',
+      author: 'Murasaki Shikibu',
+      description: 'est une œuvre considérée comme majeure de la littérature japonaise du xie siècle, attribuée à Murasaki Shikibu. Comme avec la plupart des ouvrages de l'époque de Heian, le Dit du Genji a probablement été écrit principalement (ou peut-être entièrement) en caractères kana (écriture phonétique japonaise) et non en caractères chinois, car il a été écrit par une femme pour un public féminin.'
+      ,imageUrl: 'https://books.google.tn/books/content?id=rONYswEACAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE71g0j8CUNfAQ2D9o_pCk6JLBj_6eVJOorhMaqBk2Sei7HDBddcEMXe7ttOEnulVpp7uzZLqMHN_fxxLSnYiEQgy6lahgE1Mm3PIH-DSTADJBjCtNy1l3pfNl3fMyno8RpXbMiem',id: 1, price: 12.9,quantity:1)
+  ,
+   Book(
+      title: 'L'Homme sans qualités',
+      author: 'Robert Musil',
+      description: 'est un roman inachevé de l'écrivain autrichien Robert Musil. Le premier tome parut en 1930, la première partie du deuxième en 1932.
+
+Le nazisme priva ensuite Robert Musil de ses lecteurs et de ses revenus : de Berlin, il émigra à Vienne, puis, après l’Anschluss, en Suisse. Ruiné, malade, il ne parvint pas à achever son roman et les manuscrits qu'il a laissés ne permettent pas de voir quel scénario il envisageait pour la fin.'
+      ,imageUrl: 'https://books.google.com/books?id=ezqe1hh91q4C&pg=PR3&img=1&zoom=5&sig=bBmzIAIiCtMcM7Ii7TUHycqqEWg',id: 2, price: 16.5,quantity:1)
+  ,
+  Book(
+      title: 'Les Métamorphoses',
+      author: 'Ovide',
+      description: 'sont un long poème latin d'Ovide, dont la composition débute probablement en l'an 1. L'œuvre comprend quinze livres (près de douze mille vers) écrits en hexamètres dactyliques et regroupe plusieurs centaines de récits courts sur le thème des métamorphoses issus de la mythologie grecque et de la mythologie romaine, organisés selon une structure complexe et souvent imbriqués les uns dans les autres. La structure générale du poème suit une progression chronologique, depuis la création du monde jusqu'à l'époque où vit l'auteur, c'est-à-dire le règne de l'empereur Auguste.',
+      imageUrl: 'https://books.google.com/books?id=gfg13CM_kU8C&pg=PP1&img=1&zoom=5&sig=LsTwGVAsy_qWYMPM6HVDTPAMokg',id: 3, price: 11,quantity:1),
+
+      
+  Book(
+      title: 'Les Enfants de minuit',
+      author: '	Salman Rushdie',
+      description: 'est un roman en anglais de Salman Rushdie. Il est paru en avril 1981 chez Jonathan Cape au Royaume-Uni. La traduction en français paraît en 1983 aux éditions Stock dans une traduction de Jean Guiloineau.',
+      imageUrl: 'https://books.google.com/books/content?id=iwiYGwAACAAJ&printsec=frontcover&img=1&zoom=5&sig=_L6ySKDAs-8gNK28c3NyFdO22ZM',id: 4, price: 22,quantity:1),
+
+];
